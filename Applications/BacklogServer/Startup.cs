a43ee9d87992d62db357b4f3ffed1e207088c2e5
avlakshmi77 <avlakshmi77@gmail.com>
2020-04-30 06:37:24 +0000

    Added ircuit breaker to  distributed app

--- Applications/BacklogServer/Startup.cs
+++ Applications/BacklogServer/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Logging;
 using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
 using Steeltoe.Discovery.Client;
 using Steeltoe.Common.Discovery;
+using Steeltoe.CircuitBreaker.Hystrix;
 
 namespace BacklogServer
 {
@@ -44,7 +45,9 @@ namespace BacklogServer
                     BaseAddress = new Uri(Configuration.GetValue<string>("REGISTRATION_SERVER_ENDPOINT"))
                 };
 
-                return new ProjectClient(httpClient);
+                // return new ProjectClient(httpClient);
+                var logger = sp.GetService<ILogger<ProjectClient>>();
+                return new ProjectClient(httpClient, logger);
             });
             services.AddDiscoveryClient(Configuration);
         }
@@ -54,6 +57,8 @@ namespace BacklogServer
         {
             app.UseMvc();
             app.UseDiscoveryClient();
+            app.UseHystrixMetricsStream();
+            app.UseHystrixRequestContext();
         }
     }
 }
